7c5aa40158350444f2e19bdae8840826f19aafde
JuiceFactory22 <will@juicefactorydigital.com>
2026-08-31 14:35:04 -0400

    delete old folders

--- lead-desk (3)/lib/db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-// Reuse the Prisma client across hot reloads in dev so we don't
-// exhaust database connections.
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const db = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
